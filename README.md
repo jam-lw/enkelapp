a2c48c43e0b571f60d6ba1420af3a67f2976d9e7
Johan Alm <jam@learningwell.se>
2021-08-12 14:34:15 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Enkelapp
+
+A simple Next.js app
+
+## Dev
+
+- `npm install`
+- `npm run dev`
+
+## Prod
+
+- `npm install`
+- `npm run build`
+- `npm run start`
---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+export const IndexPage = () => (
+  <>
+    <main>
+      <h1>Hello</h1>
+    </main>
+    <style jsx>{`
+      main {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100vw;
+        height: 100vh;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+          Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+          "Segoe UI Symbol";
+      }
+    `}</style>
+  </>
+);
+
+export default IndexPage;
